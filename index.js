b3c3944215ccaefe333755247988a6eab39f91a4
WSINTRA <will.simpson85@gmail.com>
2019-06-27 13:42:03 -0400

    firday finishes

--- index.js
+++ index.js
@@ -15,7 +15,11 @@
 
         // Set line width
 //helper function
+//
 
+function AABBIntersect(ax, ay, aw, ah, bx, by, bw, bh) {
+   return ax < bx+bw && bx < ax+aw && ay < by+bh && by < ay+ah; 
+}
 
 //Bullet
 function Bullet(x, y , velocity, w, h, color) {
